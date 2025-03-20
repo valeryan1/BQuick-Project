e829108d99c4fb2c7db4e222395cffb6a672d06b
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2025-03-20 09:45:45 +0700

    Add RFQController and RFQ Index view

---
+++ BQuick/Controllers/RFQController.cs
@@ -0,0 +1,21 @@
+﻿using BQuick.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BQuick.Controllers
+{
+    public class RFQController : Controller
+    {
+        public readonly BQuickDbContext _context;
+
+        public RFQController(BQuickDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.RFQs.Include(r => r.Customer).ToListAsync());
+        }
+    }
+}
