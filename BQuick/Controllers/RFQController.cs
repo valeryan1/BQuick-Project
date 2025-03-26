4515cd39e5fe8f04afcb3fd99530f1a17cd3c50e
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2025-03-26 16:01:58 +0700

    Refactor site.css, fix footer overflow, and update Create RFQ page

--- BQuick/Controllers/RFQController.cs
+++ BQuick/Controllers/RFQController.cs
@@ -24,73 +24,7 @@ namespace BQuick.Controllers
 
         public IActionResult Create()
         {
-            ViewBag.Customers = new SelectList(_context.Customers, "CustomerID", "CompanyName");
-            ViewBag.Users = new SelectList(_context.Users, "UserID", "FullName");
-            ViewBag.RFQCode = GenerateRFQCode();
-
             return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("RFQName,CustomerID,RequestDate,DueDate,AssignedTo,Category,TotalBudget")] RFQ rfq, List<RFQItem> RFQItems, IFormFile attachment)
-        {
-            if (ModelState.IsValid)
-            {
-                rfq.RFQCode = GenerateRFQCode();
-                rfq.CreatedAt = DateTime.Now;
-                rfq.Status = "Open";
-
-                _context.Add(rfq);
-                await _context.SaveChangesAsync();
-
-                foreach (var item in RFQItems)
-                {
-                    item.RFQID = rfq.RFQID;
-                    _context.Add(item);
-                }
-
-                if (attachment != null)
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        await attachment.CopyToAsync(memoryStream);
-
-                        var attachmentEntity = new Attachment
-                        {
-                            RelatedID = rfq.RFQID,
-                            RelatedType = "RFQ",
-                            FileName = attachment.FileName,
-                            FileType = attachment.ContentType,
-                            FileContent = memoryStream.ToArray(),
-                            UploadedAt = DateTime.Now,
-                            UploadedBy = rfq.CreatedBy
-                        };
-
-                        _context.Add(attachmentEntity);
-                    }
-                }
-
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            ViewBag.Customers = new SelectList(_context.Customers, "CustomerID", "CompanyName", rfq.CustomerID);
-
-            ViewBag.Users = new SelectList(_context.Users, "UserID", "FullName", rfq.AssignedTo);
-
-            return View(rfq);
-        }
-
-        private string GenerateRFQCode()
-        {
-            var year = DateTime.Now.ToString("yy");
-
-            var lastRFQ = _context.RFQs.OrderByDescending(r => r.RFQID).FirstOrDefault();
-            var sequenceNumber = (lastRFQ?.RFQID ?? 0) + 1;
-
-            return $"RFQ{year}{sequenceNumber:D4}";
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
