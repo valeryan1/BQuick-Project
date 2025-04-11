431f7e6902c6565ffcf3430e130141365417195b
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2025-04-11 17:04:48 +0700

    Update Customer Address Details form fields and add Customer Contact buttons

--- BQuick/wwwroot/js/site.js
+++ BQuick/wwwroot/js/site.js
@@ -7,12 +7,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const hamburger = document.querySelector(".toggle-btn");
-const toggler = document.querySelector("#icon");
+const hamburger = document.querySelector(".toggle-btn")
+const toggler = document.querySelector("#icon")
 hamburger.addEventListener("click", function () {
     document.querySelector("#sidebar").classList.toggle("expand");
-    toggler.classList.toggle("bx-chevrons-right");
-    toggler.classList.toggle("bx-chevrons-left");
+    toggler.classList.toggle("bx-chevrons-right")
+    toggler.classList.toggle("bx-chevrons-left")
 })
 
 document.querySelector("#create-customer-option").addEventListener("click", function () {
@@ -25,3 +25,45 @@ document.querySelector(".add-customer-form-close-btn").addEventListener("click",
     document.body.classList.remove("popup-active")
 })
 
+var customerAddressDetailsCheckbox = document.querySelector("#customer-address-details-checkbox")
+customerAddressDetailsCheckbox.addEventListener("change", function () {
+    const customerAddressDetails = document.querySelectorAll(".customer-address-details");
+
+    if (this.checked) {
+        customerAddressDetails.forEach(element => {
+            element.classList.add("active")
+        });
+    } else {
+        customerAddressDetails.forEach(element => {
+            element.classList.remove("active")
+        })
+    }
+})
+
+var matchCustomerBillingAddressCheckbox = document.getElementById("match-customer-billing-address-checkbox");
+var customerBillingAddressFields = [
+    "address", "street", "city", "province", "country", "zip-code"
+]
+function updateCustomerShippingAddress() {
+    if (matchCustomerBillingAddressCheckbox.checked) {
+        customerBillingAddressFields.forEach(field => {
+            var customerBillingAddressValue = document.getElementById("customer-billing-${field}").value
+            document.getElementById("customer-shipping-${field}").value = customerBillingAddressValue
+        })
+    }
+}
+matchCustomerBillingAddressCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+        updateCustomerShippingAddress()
+        customerBillingAddressFields.forEach(field => {
+            document.getElementById("customer-shipping-${field}").disabled = true
+        })
+    } else {
+        customerBillingAddressFields.forEach(field => {
+            document.getElementById("customer-shipping-${field}").disabled = false
+        })
+    }
+});
+customerBillingAddressFields.forEach(field => {
+    document.getElementById("customer-billing-${field}").addEventListener("input", updateCustomerShippingAddress);
+});
