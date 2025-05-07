82aacacaca704898c41863e793a655355ca33107
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2025-05-07 17:09:27 +0700

    Add Request Table

---
+++ BQuick/Controllers/RequestController.cs
@@ -0,0 +1,28 @@
+﻿using BQuick.Data;
+using BQuick.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BQuick.Controllers
+{
+    public class RequestController : Controller
+    {
+        public readonly BQuickDbContext _context;
+
+        public RequestController(BQuickDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
